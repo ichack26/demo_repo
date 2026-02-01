964ad9f6bc7b35d5d0fca25e8000ac635463c4dc
Dylan Austin <da1425@ic.ac.uk>
2026-02-01 00:42:53 +0000

    first demo

--- test1.py
+++ test1.py
@@ -1,6 +1,8 @@
+from test import test1_fun1
+
 def fun1(x):
-    return x + 2
+    return x + test1_fun1(x)
 
 def fun2(x):
-    return x + 3
+    return x + 4
 
---
+++ test2.py
@@ -0,0 +1,3 @@
+def test1_fun1(x):
+    print(x + 3)
+    return x + 1 + 2
